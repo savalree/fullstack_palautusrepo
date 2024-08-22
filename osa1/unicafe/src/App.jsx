d2d4a6b2145b87abde422b6b82a1b116be74f82d
Reetta Savaloja <reetta.savaloja@tietoevry.com>
2024-08-22 07:36:43 +0300

    tehtävä 1.7

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -11,19 +11,41 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [mean, setMean] = useState(0)
+  const [positives, setPositives] = useState(0)
+
 
   const handleGoodClick = () => {
     const updatedValue = good + 1
     setGood(updatedValue)
+    const newTotal = updatedValue + neutral + bad
+    setTotal(newTotal)
+    const updatedMean = mean + 1
+    setMean(updatedMean)
+    console.log(updatedValue, newTotal, updatedValue/newTotal)
+    setPositives(updatedValue/newTotal * 100)
+    console.log(positives)
   }
 
   const handleNeutralClick = () => {
     const updatedValue = neutral + 1
     setNeutral(updatedValue)
+    const newTotal = updatedValue + good + bad
+    setTotal(newTotal)
+    setPositives(good/newTotal * 100)
+    console.log(positives)
   }
+
   const handleBadClick = () => {
     const updatedValue = bad + 1
     setBad(updatedValue)
+    const newTotal = updatedValue + neutral + good
+    setTotal(newTotal)
+    const updatedMean = mean - 1
+    setMean(updatedMean)
+    setPositives(good/newTotal * 100)
+    console.log(positives)
   }
 
   return (
@@ -36,7 +58,9 @@ const App = () => {
     <p>good {good}</p>
     <p>neutral {neutral}</p>
     <p>bad {bad}</p>
-
+    <p>all {total}</p>
+    <p>average {mean/total}</p>
+    <p>positive {positives} %</p>
     </div>
   )
 }
