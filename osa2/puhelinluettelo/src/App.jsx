692ae76a436795b20299439a4f0a3c50275b542d
Reetta Savaloja <reetta.savaloja@tietoevry.com>
2024-08-23 14:50:18 +0300

    tehtävä 2.6

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNewName = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNewName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person =>
+          <p key={person.name}>{person.name}</p>
+        )}
+      </ul>
+    </div>
+  )
+
+}
+
+export default App
