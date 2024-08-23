88c88f2faea037957fd96e91e2be2ade49ae37fe
Reetta Savaloja <reetta.savaloja@tietoevry.com>
2024-08-23 15:02:40 +0300

    tehtävä 2.7

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -11,8 +11,13 @@ const App = () => {
     const nameObject = {
       name: newName
     }
-    setPersons(persons.concat(nameObject))
-    setNewName('')
+    
+    if(persons.some(person => person.name === newName)){
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons(persons.concat(nameObject))
+      setNewName('')
+    }
   }
 
   const handleNewName = (event) => {
