66745e36c04e1b40e27f9b3bf85f3fbc674fe538
Reetta Savaloja <reetta.savaloja@tietoevry.com>
2024-08-26 15:56:08 +0300

    tehtävä 2.12

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -55,10 +55,15 @@ const App = () => {
     if(persons.some(person => person.name === newName)){
       alert(`${newName} is already added to phonebook`)
     } else {
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+      axios
+      .post('http://localhost:3001/persons', personObject)
+      .then(response => {
+        setPersons(persons.concat(personObject))
+        setNewName('')
+        setNewNumber('')
+      })
     }
+
   }
 
   const filterPersons = filterParameter
