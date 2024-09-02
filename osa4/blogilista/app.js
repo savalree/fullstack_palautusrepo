add81af967c37d4c370a98d9fefee2f62bd2ac52
Reetta Savaloja <reetta.savaloja@tietoevry.com>
2024-09-02 11:57:20 +0300

    tehtävä 4.2

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,32 @@
+const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
