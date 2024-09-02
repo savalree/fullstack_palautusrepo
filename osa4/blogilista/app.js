56e39e68485d28a72e8907bb05bce1d8417f241f
Reetta Savaloja <reetta.savaloja@tietoevry.com>
2024-09-02 12:06:34 +0300

    tehtävä 4.3

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -13,10 +13,10 @@ const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
   .then(() => {
-    console.log('connected to MongoDB')
+    logger.info('connected to MongoDB')
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
+    logger.error('error connecting to MongoDB:', error.message)
   })
 
 
--- osa4/blogilista/models/blog.js
+++ osa4/blogilista/models/blog.js
@@ -15,5 +15,4 @@ blogSchema.set('toJSON', {
   }
 })
 
-  
 module.exports = mongoose.model('Blog', blogSchema)
