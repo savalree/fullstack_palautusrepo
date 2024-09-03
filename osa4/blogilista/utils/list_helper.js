5d10e334ffc1edc09b85f8fb4b116c992bbe84c8
Reetta Savaloja <reetta.savaloja@tietoevry.com>
2024-09-03 08:04:24 +0300

    tehtävä 4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -11,8 +11,22 @@ const totalLikes = (blogs) => {
 
     return(sumOfLikes)
 }
+
+const mostLikes = (blogs) => {
+
+    const mostLikes = blogs.reduce(
+        (prev, current) => {
+            return prev.likes > current.likes ? prev : current
+        }
+    )
+
+    const { __v, _id, url, ...mostLiked} = mostLikes
+
+    return mostLiked
+}
   
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    mostLikes
 }
