08fee92f3762d67709bfa6bb47c8b5b1c20024a4
Reetta Savaloja <reetta.savaloja@tietoevry.com>
2024-09-09 08:25:16 +0300

    tehtävä 4.18

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('./controllers/blogs')
+const loginRouter = require('./controllers/login')
 const usersRouter = require('./controllers/users')
 const config = require('./utils/config')
 const express = require('express')
@@ -27,6 +28,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
