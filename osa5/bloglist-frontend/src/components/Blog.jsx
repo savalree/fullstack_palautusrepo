011eb873724fac77e5371a45a8f76db319c84272
Reetta Savaloja <reetta.savaloja@tietoevry.com>
2024-09-12 09:07:45 +0300

    tehtävä 5.7

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleText = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+
+return (
+  <div style={blogStyle}>
+    <div> 
+      "{blog.title}" by {blog.author} <button onClick={toggleText}>{isExpanded ? 'hide' : 'view'}</button>
+    </div>
+    {isExpanded && (
+      <div>
+        {blog.url} <br></br>
+        {blog.likes} <button>like</button> <br></br>
+        {blog.user.username}
+      </div>
+    )}
+  </div>
+)}
 
 export default Blog
