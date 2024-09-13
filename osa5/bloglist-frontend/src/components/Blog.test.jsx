c3878cf52702bfda997919459b66d437a9a1c0a9
Reetta Savaloja <reetta.savaloja@tietoevry.com>
2024-09-13 13:59:08 +0300

    5.14

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
+
 
 test('renders title', () => {
   const blog = {
@@ -13,5 +15,28 @@ test('renders title', () => {
   render(<Blog blog={blog} />)
 
   const element = screen.getByText('TestiRenderöinti by SuperTest')
+  expect(element).toBeDefined()
+})
+
+test('clicking view button shows url, likes and user', async () => {
+  const blog = {
+    title: 'TestiRenderöinti',
+    author: 'SuperTest',
+    url: 'www.testi.fi',
+    likes: 0,
+    user: {
+      username: 'testi'
+    }
+  }
+
+  render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const element = screen.getByText('www.testi.fi 0 testi')
+
   expect(element).toBeDefined()
 })
