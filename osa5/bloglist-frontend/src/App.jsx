3d71d3d2cf0a3e74dbe85042c37c8490082f1059
Reetta Savaloja <reetta.savaloja@tietoevry.com>
2024-09-16 13:26:22 +0300

    tehtävä 5.18

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -57,6 +57,7 @@ const App = () => {
       <div>
         username
         <input
+          data-testid='username'
           type="text"
           value={username}
           name="Username"
@@ -66,6 +67,7 @@ const App = () => {
       <div>
         password
         <input
+          data-testid='password'
           type="password"
           value={password}
           name="Password"
