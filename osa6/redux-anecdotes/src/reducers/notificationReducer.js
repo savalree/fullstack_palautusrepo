71c9a3dca8df11f291438017c9a4fdfafa32c3de
Reetta Savaloja <reetta.savaloja@tietoevry.com>
2024-09-19 09:13:14 +0300

    tehtävä 6.12

---
+++ osa6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const notificationSlice = createSlice({
+    name: 'notification',
+    initialState: 'So important much notification',
+    reducers: {
+      setNotification: (state, action) => {
+        console.log('Notif ACTION: ', action)
+        console.log('Notif state', state)
+        console.log("Notif is", action.payload)
+        return action.payload
+      }
+    }
+  })
+  
+  export const { setNotification } = notificationSlice.actions
+  
+  export default notificationSlice.reducer
